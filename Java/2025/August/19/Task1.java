14ea921362cd5452328caf24998b8bb04e069dc7
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-08-19 06:47:08 +0000

    Day 19 work - Add Action.md - Add Question.md - Add Task.md - Add code for int and string variables.

---
+++ Java/2025/August/19/Task1.java
@@ -0,0 +1,6 @@
+class Task1 {
+    public static void main(String [] args){
+        int a = 1;
+        System.out.println("The value is "+a);
+    }
+}
---
+++ Java/2025/August/19/Task2.java
@@ -0,0 +1,6 @@
+class Task2{
+    public static void main(String[] args){
+        String name = "Mehdi";
+        System.out.println("My name is "+name);
+    }
+}
