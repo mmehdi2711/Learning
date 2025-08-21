26d369ffa7eecc7a9f9b831583ca1cfed448cc6d
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-08-21 05:46:38 +0000

    Create Task1.java

---
+++ Java/2025/August/21/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+
+    public static void main(String []args){
+        System.out.println("5 X 1 = 5");
+        System.out.println("5 X 2 = 10");
+        System.out.println("5 X 3 = 15");
+        System.out.println("5 X 4 = 20");
+        System.out.println("5 X 5 = 25");
+    }
+}
