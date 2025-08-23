12ab69de0cd893449059cc9b1b3141c0b4512522
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-08-23 06:12:05 +0000

    Make Task1.java

---
+++ Java/2025/August/22/Task1.java
@@ -0,0 +1,9 @@
+public class Task1{
+
+    public static void main(String[] args) {
+        int a = 98;
+        System.out.printf("%d X %d = %d", 5, 1, 5 * 1).println();
+        System.out.printf("%d X %d = %f", 5, 2, 5.0).println();
+        System.out.printf("%d X %d = %c", 5, 3, a).println();
+    }
+}
