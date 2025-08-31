2166362d0482b416b59ffd34b03f910ffa4dc393
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-08-31 09:29:42 +0000

    Create Aug31.java

---
+++ Java/2025/August/29/Aug29.java
@@ -0,0 +1,17 @@
+public class Aug29{
+
+    public static void main(String[] args) {
+        int i= 6;
+        System.out.println(i);
+        System.out.println(i<7);
+        System.out.println(i>7);
+        if (i>7) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
+    
+
