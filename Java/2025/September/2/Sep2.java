3aa472d752fb62436ecfc6438c5ec121f2f183aa
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-09-03 15:28:12 +0000

    Added code output after modifying Sep2.java

--- Java/2025/September/2/Sep2.java
+++ Java/2025/September/2/Sep2.java
@@ -2,7 +2,20 @@ public class Sep2{
 
   public static void main(String[] args) {
     for (int i = 1; i <= 10; i++) {
+        System.out.printf("%d X %d = %d", 5, i, 5 * i);
       System.out.println("The current value of i is: " + i);
     }
   }
 }
+
+Code output:
+5 X 1 = 5The current value of i is: 1
+5 X 2 = 10The current value of i is: 2
+5 X 3 = 15The current value of i is: 3
+5 X 4 = 20The current value of i is: 4
+5 X 5 = 25The current value of i is: 5
+5 X 6 = 30The current value of i is: 6
+5 X 7 = 35The current value of i is: 7
+5 X 8 = 40The current value of i is: 8
+5 X 9 = 45The current value of i is: 9
+5 X 10 = 50The current value of i is: 10
