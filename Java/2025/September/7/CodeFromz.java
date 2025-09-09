32f943d12eb4575242c9927be33aad27e2a55417
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-09-09 13:58:09 +0000

    Checking for errors

---
+++ Java/2025/September/7/CodeFromz.java
@@ -0,0 +1,7 @@
+
+void sayHelloWorldTwice() {
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+}
+
+//There's no output, this was asked to be copied.
---
+++ Java/2025/September/7/Notes.md
@@ -0,0 +1,6 @@
+1. Below is the format for using methods.
+```
+ReturnType nameOfTheMethod(){
+    //Body of the Method
+}
+```
