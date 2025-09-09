ea397bb0108150a324f17ac0626f0b4c4e9868da
Muhammad Mehdi Hannure <93570450+heheboyy27@users.noreply.github.com>
2025-09-09 13:49:43 +0000

    Checking grammar and fixing output error

---
+++ Java/2025/September/6/ActionSep6.md
@@ -0,0 +1,2 @@
+1. Practiced using for loop along with variables and operators to square values. (little practice project without using videos)
+2. COMPLETED SECTION 3 AND EXAM (100%) (yay).
---
+++ Java/2025/September/6/SquareNumbers.java
@@ -0,0 +1,29 @@
+public class SquareNumbers {
+    public static void main(String[] args) {
+        for (int i = 20; i >= 1; i--) {
+            System.out.println(i + " squared is " + (i * i));
+        }
+    }
+}
+
+// Code output:
+//20 squared is 400
+//19 squared is 361
+//18 squared is 324
+//17 squared is 289
+//16 squared is 256
+//15 squared is 225
+//14 squared is 196
+//13 squared is 169
+//12 squared is 144
+//11 squared is 121
+//10 squared is 100
+//9 squared is 81
+//8 squared is 64
+//7 squared is 49
+//6 squared is 36
+//5 squared is 25
+//4 squared is 16
+//3 squared is 9
+//2 squared is 4
+//1 squared is 1
