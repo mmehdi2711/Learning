375a612e4dd636c85b16addf8461a719fb329f06
heheboyy27 <mmehdi2711@gmail.com>
2025-09-21 16:28:33 +0000

    practice files

---
+++ Java/2025/September/22/ScholarshipSep22.py
@@ -0,0 +1,12 @@
+grade = int(input("What is your grade? (number only): "))
+average_score = int(input("What is your average score? (number only): "))
+
+if grade >= 12:
+    if average_score >= 90:
+        print("You qualify for a full scholarship!")
+    elif average_score >=89>75:
+        print("You qualify for a partial scholarship.")
+    else:
+        print("You do not qualify for a scholarship.")
+else:
+    print("You do not qualify for a scholarship, due to being under Grade 12.")
